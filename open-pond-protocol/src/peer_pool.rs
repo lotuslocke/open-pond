df305a28db66b6e279e6e751ce08fefdedfa46ff
William Greer <wgreer184@gmail.com>
2020-11-08 19:42:19 -0500

    Cleaning up from configuration development

--- open-pond-protocol/src/peer_pool.rs
+++ open-pond-protocol/src/peer_pool.rs
@@ -11,7 +11,7 @@ pub fn start_peer_pool(peers: Vec<Address>) -> std::io::Result<()> {
     loop {
         thread::sleep(time::Duration::new(1, 0));
         for (i, peer) in peers.iter().enumerate() {
-            if !active[i] {    
+            if !active[i] {
                 if let Ok(stream) = TcpStream::connect(peer.address.clone()) {
                     println!("{} accepted connection request", peer.name.clone());
                     thread::spawn(|| request(stream));
@@ -26,8 +26,7 @@ fn request(mut stream: TcpStream) -> std::io::Result<()> {
     loop {
         let mut response = [0; 1024];
         thread::sleep(time::Duration::new(1, 0));
-        if stream.read_exact(&mut response).is_ok() {
-            println!("Good");
+        if stream.read(&mut response).is_ok() {
             println!("Response: {}", std::str::from_utf8(&response).unwrap());
         }
     }
